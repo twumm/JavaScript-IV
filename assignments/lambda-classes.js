630caf54665faa96a0b5a2d07dbef67328970063
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-09 12:18:44 +0000

    feature: Add Instructor class that extends the Person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -13,3 +13,20 @@ class Person {
     console.log(`Hello my name is ${this.name}, I am from ${this.Bedrock}.`)
   }
 }
+
+class Instructor extends Person {
+  constructor(props){
+    super(props)
+    this.specialty = props.specialty;
+    this.favLanguage = props.favLanguage;
+    this.catchPhrase = props.catchPhrase;
+  }
+
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}`)
+  }
+
+  grade(student, subject) {
+    console.log(`${student.name} receives a perfect score on ${subject}`)
+  }
+}
