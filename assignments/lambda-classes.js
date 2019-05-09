f08e7bcf74585fa686e5f21730587a4fd79d3c79
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-09 12:13:04 +0000

    feature: Create Person class - tje base class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,2 +1,15 @@
 // CODE here for your Lambda Classes
 
+// Base class
+class Person {
+  constructor(props) {
+    this.name = props.name;
+    this.age = props.age;
+    this.location = props.location;
+    this.gender = props.gender;
+  }
+
+  speak() {
+    console.log(`Hello my name is ${this.name}, I am from ${this.Bedrock}.`)
+  }
+}
